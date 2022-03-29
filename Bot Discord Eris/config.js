321d273d12e059e2a26a449f466121d21d97391b
NEYFOX <92888341+NEYFOXX@users.noreply.github.com>
2022-03-29 19:25:15 +0200

    Update config.js

--- Bot Discord Eris/config.js
+++ Bot Discord Eris/config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    token: "OTM3MDI2NjU5NjQxNjAyMTg4.YfVwFA.9JDjtpXStfeAnEBGj9cD4W0K42w",
+    token: "give your token here",
     database: {
         user: "root", 
-        password: "neyfox15",
-        database: "eris"
+        password: "give your password here",
+        database: "give the database name here"
     }
 }
